fdad8fe9bbe9b2caae1bd1801d69bb8f9597f36f
Varada Kolhatkar <kvarada@cs.ubc.ca>
2025-06-23 09:29:45 -0700

    Built site for gh-pages

--- slides/code/plotting_functions.py
+++ slides/code/plotting_functions.py
@@ -6,11 +6,15 @@ from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
 from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.model_selection import cross_validate, train_test_split
 from sklearn.svm import SVC
-from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.pipeline import make_pipeline
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 import imageio
 
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import (
+    cross_validate,
+)
 
 
 # adapted from mglearn https://github.com/amueller/mglearn/blob/master/mglearn/tools.py
@@ -553,4 +557,84 @@ def plot_multiclass_lr_ovr(lr, X_train, y_train, n_classes, test_points=None, de
             plt.plot(test_point[0], test_point[1], "k*", markersize=16)
     if decision_boundary:
         mglearn.plots.plot_2d_classification(lr, X_train, fill=True, alpha=0.7)
-        
+
+# Code credit: Adapted from 
+# https://learning.oreilly.com/library/view/introduction-to-machine/9781449369880/
+
+def eval_on_features(features, target, regressor, xticks, 
+                     n_train=184, sales_data=False,                      
+                     ylabel='Rentals', 
+                     feat_names="Default", 
+                     impute=True):
+    """
+    Evaluate a regression model on a given set of features and target.
+
+    This function splits the data into training and test sets, fits the 
+    regression model to the training data, and then evaluates and plots 
+    the performance of the model on both the training and test datasets.
+
+    Parameters:
+    -----------
+    features : array-like
+        Input features for the model.
+    target : array-like
+        Target variable for the model.
+    regressor : model object
+        A regression model instance that follows the scikit-learn API.
+    n_train : int, default=184
+        The number of samples to be used in the training set.
+    sales_data : bool, default=False
+        Indicates if the data is sales data, which affects the plot ticks.
+    ylabel : str, default='Rentals'
+        The label for the y-axis in the plot.
+    feat_names : str, default='Default'
+        Names of the features used, for display in the plot title.
+    impute : bool, default=True
+        whether SimpleImputer needs to be applied or not
+
+    Returns:
+    --------
+    None
+        The function does not return any value. It prints the R^2 score
+        and generates a plot.
+    """
+
+    # Split the features and target data into training and test sets
+    X_train, X_test = features[:n_train], features[n_train:]
+    y_train, y_test = target[:n_train], target[n_train:]
+
+    if impute:
+        simp = SimpleImputer()
+        X_train = simp.fit_transform(X_train)
+        X_test = simp.transform(X_test)
+    
+    # Fit the model on the training data
+    regressor.fit(X_train, y_train)
+
+    # Print R^2 scores for training and test datasets
+    print("Train-set R^2: {:.2f}".format(regressor.score(X_train, y_train)))
+    print("Test-set R^2: {:.2f}".format(regressor.score(X_test, y_test)))
+
+    # Predict target variable for both training and test datasets
+    y_pred_train = regressor.predict(X_train)
+    y_pred = regressor.predict(X_test)
+
+    # Plotting
+    plt.figure(figsize=(10, 3))
+
+    # If not sales data, adjust x-ticks for dates (assumes datetime format)
+    if not sales_data: 
+        plt.xticks(range(0, len(features), 8), xticks.strftime("%a %m-%d"), rotation=90, ha="left")
+
+    # Plot training and test data, along with predictions
+    plt.plot(range(n_train), y_train, label="train")
+    plt.plot(range(n_train, len(y_test) + n_train), y_test, "-", label="test")
+    plt.plot(range(n_train), y_pred_train, "--", label="prediction train")
+    plt.plot(range(n_train, len(y_test) + n_train), y_pred, "--", label="prediction test")
+
+    # Set plot title, labels, and legend
+    title = regressor.__class__.__name__ + "\n Features= " + feat_names
+    plt.title(title)
+    plt.legend(loc=(1.01, 0))
+    plt.xlabel("Date")
+    plt.ylabel(ylabel)
